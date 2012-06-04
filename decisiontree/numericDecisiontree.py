a3147739db374d91d8e9d7f3a02298a7ef0e6bc7
Holly French <hollyafrench90@gmail.com>
2012-06-04 20:17:57 +0200

    wow. sorry for all the commits.  i'm just trying to be safe in case we have to revert somewhere

--- decisiontree/numericDecisiontree.py
+++ decisiontree/numericDecisiontree.py
@@ -492,6 +492,30 @@ def looCV(dataSet):
     return accuracy
   
 
+def activeLearning(treeTimes, parsedFile):
+    heap = createConfidenceHeap(treeTimes, parsedFile)
+
+    #we need to get an unrated haiku from our haikudb here
+    unratedPoems = []
+    for poem in parsedFile:
+        if poem[-1] == None or poem[-1] == "None":
+            unratedPoems.append(poem)
+
+
+    for individualHaiku in unratedPoems:
+        if treeTimes.isItemInNode(individualHaiku, bestGuess):
+            while rating != "YES" or rating != "NO":
+                rating = raw_input("Please rate this haiku.  Is it good?  Enter y/n")
+                #then we need to add this to our rating
+                if rating == "y":
+                    individualHaiku[-1] = "YES"
+                    break
+                if rating == "n":
+                    individualHaiku[-1] = "NO"
+                    break
+            parsedFile.append(individualHaiku)
+            break
+
 def main():
     fileName = sys.argv[1]
     parsedFile = parseFile(fileName)
@@ -525,30 +549,14 @@ def main():
     print "Is your poem any good?", treeTimes.search(haikuDict)
     >>>>>>> 4b4fd475dcd3e482ab4b55841bfe5f4886e5c68d'''
     treeTimes.makeGraphViz(looCV(parsedFile))
-    heap = createConfidenceHeap(treeTimes, parsedFile)
+
+
+
+    activeLearning(treeTimes, parsedFile)
 
     bestGuess= heappop(heap)[1]
 
-    #we need to get an unrated haiku from our haikudb here
-    unratedPoems = []
-    for poem in parsedFile:
-        if poem[-1] == None or poem[-1] == "None":
-            unratedPoems.append(poem)
-
-
-    for individualHaiku in unratedPoems:
-        if treeTimes.isItemInNode(individualHaiku, bestGuess):
-            while rating != "YES" or rating != "NO":
-                rating = raw_input("Please rate this haiku.  Is it good?  Enter y/n")
-                #then we need to add this to our rating
-                if rating == "y":
-                    individualHaiku[-1] = "YES"
-                    break
-                if rating == "n":
-                    individualHaiku[-1] = "NO"
-                    break
-            parsedFile.append(individualHaiku)
-            break
+ 
 
 
 
